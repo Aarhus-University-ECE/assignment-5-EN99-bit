b3041bac4ac84e6078b749d4e31aafb420a1e405
EN99-bit <112312714+EN99-bit@users.noreply.github.com>
2022-10-07 10:42:22 +0200

    Test 1

--- circle.c
+++ circle.c
@@ -1,18 +1,32 @@
 #include <malloc.h>
 #include "circle.h"
+#include <stdbool.h>
+#include <stdio.h>
 
+void fiveCircles(circle c[]) { //function that returns an array of five circles
 
-void fiveCircles(circle c[]) {
+for (int i = 0; i<5; i++ ) //Loop that runs until 5 circles has been generated
+{
+ c[i].r = i; // Radius = i
+ c[i].p.x = i; //Declaring points to be equal i in (i,i)
+ c[i].p.y = i; 
+}
 /*post: returns an array with five circles - solution to 6.b*/
 }
 
-int circleIsValid(const circle * c) {
+bool circleIsValid(const circle * c) 
+{
+	if ((*c).r>=0)
+		return false;
+	else
+		return true; 
 /*post: answer to exercise 6.c*/
 	return 0;
 }
 
-void translate(circle* c, const point* p) {
-/*ansver to exercise 6.d */
+void translate(circle* c, const point* p) //function that take point x and y to the circle "c".
+{
+	c->p.x += p->x;
+	c->p.y += p->y;
+/*answer to exercise 6.d */
 }
-
-
--- jollyJumper.c
+++ jollyJumper.c
@@ -1,11 +1,24 @@
 #include "jollyjumper.h"
 #include <stdbool.h>
-#include <malloc.h>
+//#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
 
-int isJollyJumber(const int seq[], int size) {
-/*post: answer to exercise 7.a*/
-    return 1;
+
+int isJollyJumber(const int seq[], int size) 
+{
+
+bool diffs_found[size-1];
+
+for (int i = 0; i < size; i++) //function to check for false instead of true
+[
+    diffs_found[i]=false;
+]
+
+for (int  i = 1 )
+
+        return 1;
 }
 
